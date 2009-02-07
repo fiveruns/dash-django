b8376701738679b29ca2376ce16d8ba823c7270b
Jerry Chen <github@jcsalterego.otherinbox.com>
2009-02-07 08:35:07 +0800

    Indent to 4 (consistency), added newline :)
    Signed-off-by: FiveRuns Development Team <dev@fiveruns.com>

--- fiveruns/dash/django/__init__.py
+++ fiveruns/dash/django/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.modules.has_key('django'):
-  from django.conf import settings
-  import startup
-  startup.start(settings)
+    from django.conf import settings
+    import startup
+    startup.start(settings)
